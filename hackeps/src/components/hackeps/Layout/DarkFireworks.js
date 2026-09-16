f86b3e23ecb80793938d41e0a2b9b2dfb82654a9
Aleix Rosinach <bepescode@gmail.com>
2026-09-16 21:28:24 +0200

    fix: anchor background fireworks independently of content height

--- hackeps/src/components/hackeps/Layout/DarkFireworks.js
+++ hackeps/src/components/hackeps/Layout/DarkFireworks.js
@@ -9,25 +9,25 @@ const DarkFireworks = () => (
       src={firework3}
       width={220}
       height={200}
-      className="left-[2%] top-[10%] z-0 hidden h-[90px] w-[100px] -rotate-[12deg] sm:block sm:h-[140px] sm:w-[160px] lg:h-[180px] lg:w-[200px]"
+      className="left-[2%] top-16 z-0 hidden h-[90px] w-[100px] -rotate-[12deg] sm:block sm:h-[140px] sm:w-[160px] lg:h-[180px] lg:w-[200px]"
     />
     <Firework
       src={firework1}
       width={180}
       height={170}
-      className="bottom-[14%] left-[3%] z-0 h-[70px] w-[80px] rotate-[18deg] sm:h-[120px] sm:w-[130px] lg:h-[160px] lg:w-[170px]"
+      className="top-[380px] sm:top-[420px] left-[3%] z-0 h-[70px] w-[80px] rotate-[18deg] sm:h-[120px] sm:w-[130px] lg:h-[160px] lg:w-[170px]"
     />
     <Firework
       src={firework2}
       width={200}
       height={170}
-      className="right-[3%] top-[8%] z-0 h-[70px] w-[85px] -rotate-[8deg] sm:h-[120px] sm:w-[140px] lg:h-[160px] lg:w-[190px]"
+      className="right-[3%] top-12 z-0 h-[70px] w-[85px] -rotate-[8deg] sm:h-[120px] sm:w-[140px] lg:h-[160px] lg:w-[190px]"
     />
     <Firework
       src={firework3}
       width={170}
       height={150}
-      className="bottom-[12%] right-[4%] z-0 hidden h-[70px] w-[80px] rotate-[14deg] sm:block sm:h-[110px] sm:w-[130px] lg:h-[140px] lg:w-[160px]"
+      className="top-[400px] sm:top-[440px] right-[4%] z-0 hidden h-[70px] w-[80px] rotate-[14deg] sm:block sm:h-[110px] sm:w-[130px] lg:h-[140px] lg:w-[160px]"
     />
   </>
 );
