4fac851937d5b38cf95b26205b441765cc4d46ae
Aleix Rosinach <bepescode@gmail.com>
2026-09-16 19:54:30 +0200

    fix: align user profile with the dark design reference

--- hackeps/src/components/hackeps/Profile/Profile.js
+++ hackeps/src/components/hackeps/Profile/Profile.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import "./Profile.css";
 import Modal from "react-bootstrap/Modal";
-import HSkeleton from "src/components/hackeps/LoadingSkeleton/HSkeleton";
 import { loadProfile } from "src/modules/loadProfile";
 import { clearSession } from "src/modules/session";
 import EditProfile from "./EditProfile";
@@ -16,8 +15,7 @@ import LinkAccounts from "src/components/hackeps/LinkAccounts/LinkAccounts";
 import Join from "src/components/hackeps/Join/Join";
 import QrCode from "src/components/hackeps/QrCode/QrCode.js";
 import ProfilePic from "../ProfilePic/ProfilePic";
-import Button from "src/components/buttons/Button";
-import TitleGeneralized from "../TitleGeneralized/TitleGeneralized";
+import ProfileHighlights from "./ProfileHighlights";
 
 const ProfileComponent = () => {
   const { hacker_id } = useParams();
@@ -90,141 +88,66 @@ const ProfileComponent = () => {
 
   return (
     <>
-      <div className="bg-secondaryHackeps text-white main-screen">
-        <div className="container-xxl pt-3 peter">
-          {/* User info and qr */}
-          <div className="row align-middle mx-auto mb-3">
-            {/* User Image */}
-            <div className="col-12 col-xl-4 m-auto text-center">
+      <main className="hacker-profile">
+        <div className="hacker-profile-content">
+          <header className="hacker-profile-header">
+            <div className="hacker-profile-avatar">
               <ProfilePic
                 id="profile-pic-big"
                 hacker={user}
                 size="big"
                 bgcolor="white"
-                border={true}
-                is_profile={true}
+                border
+                is_profile
               />
-              <br />
-              <br />
-              {isUser && (
-                <Link to="/home" className="logOut" onClick={logOut}>
-                  <Button secondary outline>
-                    <i className="fas fa-sign-out"></i> Tancar sessió
-                  </Button>
-                </Link>
+            </div>
+            <div className="hacker-profile-identity">
+              <p>{isUser ? "Benvingut/da, hacker!" : "Perfil de hacker"}</p>
+              <h1>{user ? user.name : "Carregant el perfil…"}</h1>
+              {user?.created_at && (
+                <p className="hacker-profile-membership">
+                  Membre des de fa {generateMemberTime(user.created_at)}
+                </p>
               )}
             </div>
+            {isUser && event?.accepted && user && (
+              <button
+                type="button"
+                className="hacker-profile-ticket"
+                onClick={handleShowQR}
+              >
+                <span>Mostra el teu tiquet</span>
+                <img src={qrIcon} alt="" width="48" height="48" />
+              </button>
+            )}
+          </header>
 
-            {/* Center Column */}
-            <div className="col-12 col-xl-4 px-0 my-3 text-center">
-              <div className="row ">
-                {isUser && (
-                  <h3 className="text-textSecondaryHackeps text-center underline decoration-primaryHackeps underline-offset-[5px] decoration-[5px] rounded pb-4">
-                    Benvingut/da, hacker!
-                  </h3>
-                )}
-              </div>
-              <div className="row my-3">
-                <div className="col-xxl-1 col-2 d-flex">
-                  <TitleGeneralized className="m-auto" normal>
-                    -
-                  </TitleGeneralized>
-                </div>
-                <TitleGeneralized className="col-xxl-10 col-8" normal>
-                  {user && user.name}
-                </TitleGeneralized>
-                <div className="col-xxl-1 col-2 d-flex">
-                  <TitleGeneralized className="m-auto" normal>
-                    -
-                  </TitleGeneralized>
-                </div>
-              </div>
-              <div className="row">
-                <span className="text-center text-textSecondaryHackeps">
-                  Membre desde fa{" "}
-                  {user ? generateMemberTime(user.created_at) : ""}
-                </span>
-              </div>
-            </div>
-            {/* QR Column */}
-            <div className="col-12 col-xl-4 mx-auto text-textSecondaryHackeps">
-              {isUser &&
-                event &&
-                event.accepted &&
-                (user ? (
-                  <div
-                    className="container qr-container text-textPrimaryHackeps bg-primaryHackeps p-2 text-center m-auto"
-                    onClick={handleShowQR}
-                  >
-                    <div className="row">
-                      <div className="col-6 my-auto col-xl-12">
-                        Mostra el teu tiquet
-                      </div>
-                      <div className="col-6 col-xl-12 my-auto">
-                        <img
-                          style={{
-                            aspectRatio: "1/1",
-                            width: "70%",
-                            fill: "black",
-                            backgroundColor: "transparent",
-                          }}
-                          className="px-2 p-0 pt-xl-4 mx-auto my-auto"
-                          src={qrIcon}
-                          alt="codi qr"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                ) : (
-                  <HSkeleton height={"100%"} />
-                ))}
-            </div>
-          </div>
-
-          {/* Accounts link */}
           {user && (
-            <>
+            <div className="hacker-profile-accounts">
               <LinkAccounts hacker={user} />
-              <br />
-            </>
-          )}
-
-          {isUser && (
-            <div className="editSpace">
-              <div className="editAjust">
-                {user && <EditProfile hackerObj={user} />}
-              </div>
             </div>
           )}
-
+          {isUser && (
+            <div className="hacker-profile-actions">
+              {user && <EditProfile hackerObj={user} />}
+              <Link to="/" className="hacker-profile-logout" onClick={logOut}>
+                Tancar sessió
+              </Link>
+            </div>
+          )}
           {isHacker && (
-            <div className="sort-horizontally">
+            <div className="hacker-profile-event">
               <Join event={event} />
             </div>
           )}
-
-          {event && event.registered && isHacker && (
-            <Team team={team} is_user={isUser} />
+          {event?.registered && isHacker && (
+            <section className="hacker-profile-team" aria-label="El teu equip">
+              <Team team={team} is_user={isUser} />
+            </section>
           )}
-
-          {/* Calendar and Achievements */}
-          <div className="row m-5 gy-5 bottom-container text-center m-auto">
-            {/* <div className="col-12 col-xl-6">
-              <Medals />
-            </div> */}
-            {/* <div className="col-12 col-xl-12 d-flex justify-content-center">
-              <div className="calendar-container mx-auto">
-                <Calendar
-                  value={[startDate, endDate]}
-                  locale={"ca"}
-                  minDetail={"month"}
-                />
-              </div>
-            </div> */}
-          </div>
-          <br />
+          {isHacker && <ProfileHighlights event={event} />}
         </div>
-      </div>
+      </main>
 
       <Modal show={showQR} onHide={handleCloseQR} centered>
         <QrCode url={qrCode} />
