c96792ac6c16f67e7fcdb62261ac2060d3edd6e8
Aleix Rosinach <bepescode@gmail.com>
2026-09-20 13:01:54 +0200

    fix: make the Seu Vella footer substantially more compact

--- hackeps/src/components/hackeps/Home/SeuVellaScene.js
+++ hackeps/src/components/hackeps/Home/SeuVellaScene.js
@@ -7,24 +7,24 @@ import firework from "src/assets/img/home10/firework-1.svg";
 export default function SeuVellaScene() {
   return (
     <svg
-      viewBox="0 0 1200 460"
+      viewBox="0 0 1200 280"
       className="block h-auto w-full"
       role="img"
       aria-label="La Seu Vella de Lleida entre arbres i turons"
       data-testid="seu-vella-scene"
     >
-      <image href={firework} x="130" y="65" width="190" height="190" />
-      <path fill="#36b39d" d="M0 265 Q420 300 1200 365 V460 H0Z" />
-      <g fill="#36b39d" transform="translate(115 155) scale(0.75)">
+      <image href={firework} x="240" y="40" width="115" height="115" />
+      <path fill="#36b39d" d="M0 160 Q420 180 1200 220 V280 H0Z" />
+      <g fill="#36b39d" transform="translate(230 94) scale(0.45)">
         <path d="M39 85 L44 196 H62 L57 85Z" />
         <path d="M14 115 C-16 112 -12 73 12 68 C7 37 28 19 46 31 C57 -7 95 14 88 42 C120 38 127 65 113 79 C147 100 121 126 101 119Z" />
       </g>
-      <g fill="#78c6bd" transform="translate(350 160)">
+      <g fill="#78c6bd" transform="translate(410 96) scale(0.6)">
         <path d="M39 85 L34 190 H62 L57 85Z" />
         <path d="M14 115 C-16 112 -12 73 12 68 C7 37 28 19 46 31 C57 -7 95 14 88 42 C120 38 127 65 113 79 C147 100 121 126 101 119Z" />
       </g>
-      <image href={seuVella} x="635" y="20" width="370" height="374" />
-      <path fill="#78c6bd" d="M0 365 Q560 285 1200 390 V460 H0Z" />
+      <image href={seuVella} x="690" y="12" width="225" height="227" />
+      <path fill="#78c6bd" d="M0 220 Q560 172 1200 236 V280 H0Z" />
     </svg>
   );
 }
